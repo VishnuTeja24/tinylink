554cea4147e7bb941cc3f0873fb05f4b382c346e
VishnuTeja24 <vishnutejamekalaa@gmail.com>
2025-11-24 17:51:09 +0530

    Fix healthz build error by removing React page

--- pages/healthz.js
+++
@@ -1,6 +0,0 @@
-export default function Health() {
-  return new Response(JSON.stringify({ ok: true, version: "1.0" }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
